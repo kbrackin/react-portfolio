2e0691a86bdb5a46afc3025b1f8b03fa15ece35b
kbrackin <135669996+kbrackin@users.noreply.github.com>
2023-11-28 00:21:52 -0500

    fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,3 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
 function Footer() {
     return (
         <footer className="footer">
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,5 +1,5 @@
 import Form from 'react-bootstrap/Form';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
 function Contact() {
